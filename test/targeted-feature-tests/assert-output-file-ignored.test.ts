3000cf1c8e4d1481edd26c6fde3b0c152e09a904
chris <chris@buildlackey.com>
2026-01-26 00:12:02 -0800

    deliberate fail to see if our build on github fails

--- test/targeted-feature-tests/assert-output-file-ignored.test.ts
+++ test/targeted-feature-tests/assert-output-file-ignored.test.ts
@@ -53,6 +53,6 @@ describe("output.filename sentinel enforcement", () => {
 
         expect(() => {
             plugin.apply(compiler);
-        }).not.toThrow();
+        }).toThrow();       // deliberate fail
     });
 });
