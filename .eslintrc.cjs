28d3f68df16d85be5d67e4b49e7e252d4108cb9f
chris <chris@buildlackey.com>
2026-01-26 20:33:18 -0800

    fix delib failed test add eslint config - add docs and notes to version ctl - es lint rules pass triggers errors a.t.m., but build/tests all pass - [does not derail build if lint errs]

---
+++ .eslintrc.cjs
@@ -0,0 +1,60 @@
+module.exports = {
+    root: true,
+    parser: "@typescript-eslint/parser",
+    plugins: ["@typescript-eslint"],
+    extends: [
+        "eslint:recommended",
+        "plugin:@typescript-eslint/recommended",
+        "plugin:@typescript-eslint/recommended-requiring-type-checking",
+    ],
+    parserOptions: {
+        project: "./tsconfig.eslint.json",
+        sourceType: "module",
+    },
+    rules: {
+        eqeqeq: ["error", "always"],
+
+        "@typescript-eslint/no-explicit-any": "error",
+
+        "@typescript-eslint/no-unsafe-assignment": "error",
+        "@typescript-eslint/no-unsafe-call": "error",
+        "@typescript-eslint/no-unsafe-member-access": "error",
+        "@typescript-eslint/no-unsafe-return": "error",
+
+        "@typescript-eslint/strict-boolean-expressions": "error",
+        "@typescript-eslint/no-floating-promises": "error",
+
+        "prefer-const": "error",
+        "no-fallthrough": "error",
+
+        "@typescript-eslint/no-shadow": "error",
+
+        "@typescript-eslint/no-unused-vars": [
+            "error",
+            { argsIgnorePattern: "^_", varsIgnorePattern: "^_" },
+        ],
+
+        "@typescript-eslint/explicit-function-return-type": [
+            "warn",
+            {
+                allowExpressions: true,
+                allowTypedFunctionExpressions: true,
+            },
+        ],
+    },
+
+    overrides: [
+        {
+            files: ["src/**/*.ts"],
+            rules: {
+                "no-undef": "off",
+                "@typescript-eslint/no-unused-vars": [
+                    "error",
+                    {
+                        varsIgnorePattern: "^(onOpen|onInstall|doGet|doPost|global)",
+                    },
+                ],
+            },
+        },
+    ],
+};
